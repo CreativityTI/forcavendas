e0dd91f020ba16942c6689144a88bb401eb469dc
Creativity <Creativity@DESKTOP-D70L6DV>
2017-05-03 14:41:39 -0300

    Revert ""
    This reverts commit 1c964d9d301d07e5aca14c3b4ae46c428ba11fe4.

--- src/main/java/com/pontorural/pedidovenda/model/ItemPedido.java
+++ src/main/java/com/pontorural/pedidovenda/model/ItemPedido.java
@@ -11,6 +11,7 @@ import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -26,9 +27,7 @@ public class ItemPedido implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @EmbeddedId
-    private PedidoPK pedidoPK;
-
+    @Id
     @ManyToOne
     @JoinColumn(name = "PEDI_PED", referencedColumnName = "PEDI_PED")
     private Pedido pedido;
@@ -46,16 +45,18 @@ public class ItemPedido implements Serializable {
     private Cfop cfop;
 
     @Column(name = "VLOR_IPE")
-    private BigDecimal valorUnitario = BigDecimal.ZERO;
+    private BigDecimal valorUnitario;
 
     @Column(name = "QTDE_IPE")
-    private Integer quantidade = 1;
+    private Integer quantidade;
 
-    @Column(name = " VLDO_IPE")
-    private BigDecimal descontoOferta;
+    public Pedido getPedido() {
+        return pedido;
+    }
 
-    @Transient
-    private BigDecimal valorTabelaPreco = BigDecimal.ONE;
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
 
     public Empresa getEmpresa() {
         return empresa;
@@ -96,81 +97,14 @@ public class ItemPedido implements Serializable {
     public void setQuantidade(Integer quantidade) {
         this.quantidade = quantidade;
     }
-
-    public BigDecimal getValorTabelaPreco() {
-        return valorTabelaPreco;
-    }
-
-    public void setValorTabelaPreco(BigDecimal valorTabelaPreco) {
-        this.valorTabelaPreco = valorTabelaPreco;
-    }
-
-    public BigDecimal getDescontoOferta() {
-        return descontoOferta = this.getValorTabelaPreco().subtract(this.valorUnitario);
-    }
-
-    public void setDescontoOferta(BigDecimal descontoOferta) {
-        this.descontoOferta = descontoOferta;
-    }
-
-    public PedidoPK getPedidoPK() {
-        return pedidoPK;
-    }
-
-    public void setPedidoPK(PedidoPK pedidoPK) {
-        this.pedidoPK = pedidoPK;
-    }
-
-    public Pedido getPedido() {
-        return pedido;
-    }
-
-    public void setPedido(Pedido pedido) {
-        this.pedido = pedido;
-    }
-
- 
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 43 * hash + Objects.hashCode(this.pedidoPK);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final ItemPedido other = (ItemPedido) obj;
-        if (!Objects.equals(this.pedidoPK, other.pedidoPK)) {
-            return false;
-        }
-        return true;
-    }
-
-    @Transient
-    public BigDecimal getValorTotalProdutos() {
-        return this.getDescontoOferta().multiply(new BigDecimal(this.getQuantidade())).add(this.valorUnitario);
-    }
+    
+    
 
     @Transient
     public BigDecimal getValorTotal() {
 
         return this.getValorUnitario().multiply(new BigDecimal(this.getQuantidade()));
-
-    }
-
-    @Transient
-    public boolean isProdutoAssociado() {
-        return this.getProduto() != null && this.getProduto().getCodigo() != null;
+        
     }
 
 }
